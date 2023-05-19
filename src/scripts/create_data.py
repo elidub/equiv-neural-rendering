afcc8ae4f41dc9fd34bb7990cbaaa4734e24cced
aeijpe <aniek.eijpe1@gmail.com>
2023-05-19 14:31:28 +0200

    Changed data creation

--- src/scripts/create_data.py
+++ src/scripts/create_data.py
@@ -7,7 +7,7 @@ def parse_option():
 
     parser.add_argument('--blender_dir', type=str, default='~/equiv-neural-rendering/src/blender/blender')
     parser.add_argument('--obj_dir', type=str, default='/project/gpuuva022/shared/equiv-neural-rendering/chairs/objects/')
-    parser.add_argument('--output_dir', type=str, default='/project/gpuuva022/shared/equiv-neural-rendering/chairs/data_half/')
+    parser.add_argument('--output_dir', type=str, default='/project/gpuuva022/shared/equiv-neural-rendering/chairs/data_all/')
     parser.add_argument('--transformation', type=str, default='rototrans', choices = ['rototrans', 'rotation', 'translation'])
 
     parser.add_argument('--n_images', type=int, default=50,
@@ -20,7 +20,7 @@ def parse_option():
                         help='Scaling that is applied to depth. Depends on size of mesh.') # ????
     parser.add_argument('--color_depth', type=str, default='8',
                         help='Number of bit per channel used for output. Either 8 or 16.') # ????
-    parser.add_argument('--resolution', type=int, default=64, # 128
+    parser.add_argument('--resolution', type=int, default=128, # 128
                         help='Resolution of the images.')
 
     argv = sys.argv[sys.argv.index("--") + 1:]
@@ -30,8 +30,9 @@ def parse_option():
 def main(args):
     files = os.listdir(args.obj_dir)
     random.shuffle(files)
+    i = 0
     for i, scene in enumerate(files):
-
+        i += 1
         scene_folder = os.path.join(args.obj_dir, scene)
 
         transformation_dict = {
@@ -45,9 +46,9 @@ def main(args):
 
         output_dir = os.path.join(args.output_dir, transf_output_dir)
 
-        if i < 2307:
+        if i < 4613:
             output_dir = os.path.join(output_dir, 'train')
-        elif i < 2638:
+        elif i < 5275:
             output_dir = os.path.join(output_dir, 'val')
         else:
             output_dir = os.path.join(output_dir, 'rest')
