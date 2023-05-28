4fa01f1d71aaa08ed891b3d614e20629209e2d1c
OlineRanum <oline.a.ranum@gmail.com>
2023-05-28 10:37:30 +0200

    Updated PSNR values

--- src/scripts/evaluate_psnr.py
+++ src/scripts/evaluate_psnr.py
@@ -9,7 +9,7 @@ from enr.misc.dataloaders import scene_render_dataset
 from enr.misc.quantitative_evaluation import get_dataset_psnr
 from enr.models.neural_renderer import load_model
 
-def evaluate(model_path, data_dir, img_res = 128, n_imgs = 64):
+def evaluate(model_path, data_dir, img_res = 128, n_source = 0):
 	device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 	# Load model
@@ -22,7 +22,7 @@ def evaluate(model_path, data_dir, img_res = 128, n_imgs = 64):
 
 	# Calculate PSNR
 	with torch.no_grad():
-	    psnrs = get_dataset_psnr(device, model, dataset, source_img_idx_shift= n_imgs,
+	    psnrs = get_dataset_psnr(device, model, dataset, source_img_idx_shift= n_source,
 		                     batch_size=1, max_num_scenes=None)
 
 	return psnrs
